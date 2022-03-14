93e7c47fabfdcaaf16d88c89b63b279b98410a8e
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-03-14 19:34:10 +0700

    Buổi 3: Tạo api đổ dữ liệu từ api ra ngoài

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import ShowInfo from './ShowInfo'
+import axios from 'axios';
+import ShowInfo from './components/ShowInfo'
+import type { ProductType } from './types/product';
+import { list, remove } from './api/product';
 
-type TProduct = {
-  id: number,
-  name: string
-}
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-  const [products, setProducts] = useState<TProduct[]>([{id: 1, name: "Dat"}])
+  // const [count, setCount] = useState<number>(0)
+
+  const [products, setProducts] = useState<ProductType[]>([])
+
+  useEffect(()=> {
+    const getProducts = async () =>{
+      const { data } = await list();
+      setProducts(data);
+    }
+    getProducts();
+  }, [])
+
+  const removeItem = async (id: number) => {
+    const { data } = await remove(id);
+    data && setProducts(products.filter(item => item.id !== data.id));
+  }
 
   return (
     <div className="App">
-        <ShowInfo name="Dat"/>
+       <table>
+          <thead>
+            <th>#</th>
+            <th>Name</th>
+            <th></th>
+          </thead>
+          <tbody>
+            {products.map((item, index) => {
+              return <tr>
+                      <td>{index + 1}</td>
+                      <td>{item.name}</td>
+                      <td>
+                        <button onClick={() => removeItem(item.id)}>Remove</button>
+                      </td>
+                    </tr>
+            })}
+          </tbody>
+        </table>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/ShowInfo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ShowInfo: React.FC<{name: string}> = ({name}) => {
-  return (
-    <div>ShowInfo</div>
-  )
-}
-
-export default ShowInfo
